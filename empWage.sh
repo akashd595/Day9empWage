01d5c32526547698fe485e552c5d16e597ba56d6
Akash Das <akashd595@gmail.com>
2022-04-14 14:58:46 +0530

    Added: part time employee

--- empWage.sh
+++ empWage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program on Master Branch"
-attendance=$((RANDOM%2));
+attendance=$((RANDOM%3));
 workingHrs=0;
 wagePerHr=20;
 dailyWage=0;
@@ -9,13 +9,20 @@ if [ $attendance -eq 0 ]
 then
    echo "Employee is Absent today";
    workingHrs=0;
+elif [ $attendance -eq 1 ]
+then
+	echo "Part Time Employee";
+	workingHrs=4;
 else
    echo "Employee is Present today";
    workingHrs=8;
 fi
-if [ $attendance -eq 1 ]
+if [ $attendance -eq 2 ]
 then
    dailyWage=$(($workingHrs*$wagePerHr));
+elif [ $attendance -eq 1 ]
+then
+	dailyWage=$(($workingHrs*$wagePerHr))
 fi
 echo "Daily Wage: $dailyWage"
 
