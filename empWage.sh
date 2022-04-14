1920587cb0024dc8416e6fe470edb66fd326e711
Akash Das <akashd595@gmail.com>
2022-04-14 10:54:00 +0530

    Added: Checking attendance

--- empWage.sh
+++ empWage.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+attendance=$((RANDOM%2))
+if [ $attendance -eq 0 ]
+then
+	echo "Employee is Absent today"
+else
+	echo "Employee is Present tpday"
+fi
