13e41e8098946bda1e9756f10453c0beb202fb23
Akash Das <akashd595@gmail.com>
2022-04-14 14:52:51 +0530

    Added: Daily Wage

--- empWage.sh
+++ empWage.sh
@@ -1,10 +1,24 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program on Master Branch"
-attendance=$((RANDOM%2))
+attendance=$((RANDOM%2));
+workingHrs=0;
+wagePerHr=20;
+dailyWage=0;
 if [ $attendance -eq 0 ]
 then
-	echo "Employee is Absent today"
+   echo "Employee is Absent today";
+   workingHrs=0;
 else
-	echo "Employee is Present tpday"
+   echo "Employee is Present today";
+   workingHrs=8;
 fi
+if [ $attendance -eq 1 ]
+then
+   dailyWage=$(($workingHrs*$wagePerHr));
+fi
+echo "Daily Wage: $dailyWage"
+
+
+
+
